c720a19803397b0e1f1d0ba26de44c41a0b73b54
Eliza <probablyeeliza@gmail.com>
2025-11-22 17:42:51 +0000

    Toolbar changes, with reactive icons

--- src/Components/Toolbar.jsx
+++ src/Components/Toolbar.jsx
@@ -1,15 +1,35 @@
 import { Link } from "react-router-dom";
 import '../Css/Toolbar.css'
+import { useState } from "react";
+import { IoMdPerson } from "react-icons/io";
+import { LuSwords } from "react-icons/lu";
+import { IoPeople } from "react-icons/io5";
+import { TbWriting } from "react-icons/tb";
+import { IoMdSettings } from "react-icons/io";
 
 // remember to remove the home 
 export function ToolBar(){
+     const [selected, setSelected] = useState(null); // initially nothing selected
+
+  const handleClick = (name) => {
+    if (selected === name) {
+      setSelected(null); // deselect if clicked again
+    } else {
+      setSelected(name); // select
+    }
+  };
     return(
         <nav className="toolbar">
-            <Link to="/"> Profile </Link>
-            <Link to="/challenge"> Challenge</Link>
-            <Link to="/community"> Community</Link>
-            <Link to="/tutorial"> Tutorial</Link>
-            <Link to="/settings"> Settings </Link>
+            <Link to="/"> <IoMdPerson size={40} className={selected === "profile" ? "toolbar-icon selected" : "toolbar-icon"}
+                onClick={() => handleClick("profile")}/> </Link>
+            <Link to="/challenge"> <LuSwords size={40} className={selected === "challenge" ? "toolbar-icon selected" : "toolbar-icon"}
+                onClick={() => handleClick("challenge")}/></Link>
+            <Link to="/community"> <IoPeople size={40} className={selected === "community" ? "toolbar-icon selected" : "toolbar-icon"}
+                onClick={() => handleClick("community")}/></Link>
+            <Link to="/tutorial"> <TbWriting size={40} className={selected === "tutorial" ? "toolbar-icon selected" : "toolbar-icon"}
+                onClick={() => handleClick("tutorial")}/></Link>
+            <Link to="/settings"> <IoMdSettings size={40} className={selected === "settings" ? "toolbar-icon selected" : "toolbar-icon"}
+                onClick={() => handleClick("settings")}/></Link>
             
         </nav>
     );
