f033c1fe9858e47b652f412479e5d3f05884f0e4
Eliza <probablyeeliza@gmail.com>
2025-11-24 20:38:46 +0000

    That previous push caused som issues with the theme so i've fixed it

--- src/Components/LightDarkModeToggle.jsx
+++ src/Components/LightDarkModeToggle.jsx
@@ -1,42 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"; 
 
-export function LightDarkModeToggle() {
-    const [darkMode, setDarkMode] = useState(false);
 
-    // If the user has a preferred theme, load it
+export function LightDarkModeToggle() { 
+    const [darkMode, setDarkMode] = useState(false); 
+    
+    // If the user has a preferred theme, load it 
     useEffect(() => {
-    const savedMode = localStorage.getItem("darkMode");
-    if (savedMode === "true") 
-        {   
-            setDarkMode(true);
-            document.documentElement.setAttribute("data-theme", savedMode);
-        } else {
-            setDarkMode(false);
-            document.documentElement.setAttribute("data-theme", "light");
-        }
-  }, []);
-
-    //Apply the theme and save preference
-    useEffect(() => {
-        if (darkMode) {
-            document.body.classList.add("dark-mode");
-            document.documentElement.setAttribute("data-theme", "dark");
-        } else {
-            document.body.classList.remove("light-mode");
-            document.documentElement.setAttribute("data-theme", "light");
-        }
-        localStorage.setItem("darkMode", darkMode);
-    }, [darkMode]);
-
-    //The button
-    return(
-        <div className="theme-container">
-        <p>Change Theme?</p>
-        <button
-            className="button"
-            onClick={() => setDarkMode(!darkMode)}>
-        {darkMode ? "Light Mode" : "Dark Mode"}
-        </button> 
-        </div>
-    );
-}
+        const savedMode = 
+            localStorage.getItem("darkMode"); 
+            if (savedMode === "true") setDarkMode(true); 
+                document.documentElement.setAttribute("data-theme", savedMode);
+         }, []); 
+    //Apply the theme and save preference 
+    useEffect(() => { 
+        if (darkMode) { 
+            document.body.classList.add("dark-mode"); 
+        } else { 
+            document.body.classList.remove("dark-mode"); } 
+        localStorage.setItem("darkMode", darkMode); },
+        [darkMode]); 
+        //The button 
+        return( 
+        <div className="theme-container"> 
+            <p>Change Theme?</p> 
+            <button className="button" 
+                onClick={() => setDarkMode(!darkMode)}> {darkMode ? "Light Mode" : "Dark Mode"} 
+                </button> 
+            </div> 
+        ); 
+    }
